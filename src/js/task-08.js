c627e4a8d492ec51c59d27cfa94e2af6be8ea44f
LiudmylaHorobets <horobets1323@gmail.com>
2023-07-22 18:16:04 +0300

    add 7-9 tasks

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,26 @@
+// Напиши скрипт управління формою логіна.
 
+// Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const { email, password } = e.currentTarget.elements;
+
+  if (!email.value.trim() || !password.value.trim()) {
+    return alert("Please fill in all the fields!");
+  } else {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    e.currentTarget.reset();
+  }
+}
